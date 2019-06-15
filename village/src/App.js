1a5c0f7b8c4a36694d95850230c2da123b6cc75c
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-14 22:47:05 -0400

    ok, I got the smurf data using axios in app.js I am commited to making more commits ;)

--- village/src/App.js
+++ village/src/App.js
@@ -3,22 +3,50 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios'; // can't to forget to import!!
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom'; // import what you need for the assignment!
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() { // using axios!!
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log("get response:", response);
+        this.setState({ smurfs: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  handleSetData = data => {
+    console.log(data, "data");
+    this.setState({ smurfs: data })
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm handleSetData={this.handleSetData} />
+        <Router>
+          <Route path="/" render={props => (
+            <Smurfs  {...props} smurfs={this.state.smurfs}
+              handleSetData={this.handleSetData}
+            />
+          )}
+          />
+        </Router>
       </div>
     );
   }
